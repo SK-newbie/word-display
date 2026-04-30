4914c45a785948b6501b1980d7f21202fb6e4b5d
Sk-Newbie <oseagwinaexcellence@gmail.com>
2026-04-30 21:18:17 +0100

    c

--- wordDisplay.js
+++ wordDisplay.js
@@ -1,3 +1,15 @@
-const heading = document.querySelector('#main-heading');
-const hiddenText = document.querySelector('.hide-text');
 const button = document.querySelector('.btn');
+const text = document.querySelector('.hide-text');
+const heading = document.querySelector('.main-heading');
+
+function showText(){
+    if (text.classList.contains('btn')){
+        text.classList.remove('btn');
+    }
+    else {
+        text.classList.add('btn');
+    }
+}
+
+button.addEventListener('click', showText)
+
